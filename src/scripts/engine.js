f67dd8f38c4927ea9a704cc05418dce5acbc3b00
Roger <rgr_147@hotmail.com>
2026-06-07 16:07:31 -0300

    inicio projeto. estilo da pagina para smartphone, horizontal, definida

---
+++ src/scripts/engine.js
@@ -0,0 +1,15 @@
+const state = {
+    score: {
+        win: 0,
+        lose: 0,   
+        scoreWin: document.getElementById('score-win'),
+        scoreLose: document.getElementById('score-lose'),
+    }
+}
+
+
+function init () {
+
+}
+
+init();
